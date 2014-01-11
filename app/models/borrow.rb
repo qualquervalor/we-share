313a4d8996c0678888ba0a7aef90ba6b3b280171
Qualquer Valor <qualquervalor@gmail.com>
2014-01-11 10:58:27 -0600

    Add relationships on models

--- app/models/borrow.rb
+++ app/models/borrow.rb
@@ -1,2 +1,4 @@
 class Borrow < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :resource
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :resources
+  has_many :borrows
+  has_many :resources, through: :borrows
 end
