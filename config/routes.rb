19e25ef3dfb82196a6b63bf352746186ff236d68
Qualquer Valor <qualquervalor@gmail.com>
2014-01-11 11:47:32 -0600

    Add test root route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ WeShare::Application.routes.draw do
 
   resources :borrows
 
+  root 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
