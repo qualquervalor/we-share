502faf439af0ae2a2512c498875eecfff4fbffad
Elaine Looney <imalooney@gmail.com>
2014-01-11 21:16:56 -0600

    update user and resource models again to correct borrowers and borrowed_resources helpers

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,7 +1,7 @@
 class Resource < ActiveRecord::Base
   belongs_to :user
   has_many :borrows
-  has_many :borrowers, class_name: "User", through: :borrow
+  has_many :borrowers, source: :user, through: :borrow
 
   mount_uploader :picture, ImageUploader
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ WeShare::Application.routes.draw do
   devise_for :users, :controllers => { registrations: :registrations }
 
   resources :users
-
+  
   resources :resources
 
   resources :borrows
