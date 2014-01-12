cdac1f18af7851df163dbcbcee1a0dc646b7ba25
Elaine Looney <imalooney@gmail.com>
2014-01-12 15:46:05 -0600

    add authenticate user as before action

--- app/controllers/borrows_controller.rb
+++ app/controllers/borrows_controller.rb
@@ -1,5 +1,6 @@
 class BorrowsController < ApplicationController
   before_action :set_borrow, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /borrows
   # GET /borrows.json
