66f9a3f44cb6b28841a8ce2796941c3a3666739f
jenscales <jen_scales@yahoo.com>
2014-01-12 13:24:26 -0600

    add public/assets/images to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,8 +26,12 @@ puts "adding dummy data for users table"
 # filepath = File.basename(URI.parse(url).path)
 
 #for our seed data we will stor the images locally and then upload them to Amazon
-filepath = "app/assets/images/shed_bg.jpg"
-snowy2 = File.open(filepath)
+ 
+
+suzy = File.open("public/assets/sue.jpb-0eeebdbe47f4aa1afbeb5ae238d2bd33.jpg") 
+ted = File.open("public/assets/Ted-3edcbe7f2115421d0ae6ddffcb4eba92.gif")
+karen = File.open("public/assets/Karen-088cea3a6f6b5e6e02fa181f74c13690.jpeg")
+jason = File.open("public/assets/Jason-b25310ece9ea99494619084bc799ad3e.jpg")
 
 users = [
         {
@@ -39,7 +43,7 @@ users = [
           city: "Austin",
           zipcode: "78701",
           state: "TX",
-          picture: snowy2
+          picture: suzy
         },
 
         {
@@ -51,7 +55,7 @@ users = [
           city: "Austin ",
           zipcode: "78701",
           state: "TX",
-          picture: snowy2
+          picture: ted
         },
 
         {
@@ -63,7 +67,7 @@ users = [
           city: "Austin",
           zipcode: "78701",
           state: "TX",
-          picture: snowy2
+          picture: jason
         },
 
         {
@@ -75,55 +79,80 @@ users = [
           city: "Austin",
           zipcode: "78701",
           state: "TX",
-          picture: snowy2
+          picture: karen
         }
 ]
 
 users.each do |user|
-  User.create!(name:user[:name], email:user[:email], password:user[:password], phone_num:user[:phone_num], street:user[:street], city:user[:city], zipcode:user[:zipcode], state:user[:state])
+  User.create!(name:user[:name], email:user[:email], password:user[:password], phone_num:user[:phone_num], street:user[:street], city:user[:city], zipcode:user[:zipcode], state:user[:state], picture:user[:picture])
 end
 
 puts "destroy resource data"
 Resource.destroy_all
 
 puts "added dummy data for resource"
+
+tilecutter = File.open("public/assets/tilecutter-0c61b39b213945eba1d1a41145004c69.jpg")
+ladder = File.open("public/assets/ladder-710be23aa20198e47f4f9788f5176d33.jpg")
+router = File.open("public/assets/router-ff5a8b1206d32dc7abfd24f0eb557930.jpg")
+tablesaw = File.open("public/assets/tablesaw-37192e882d5449ec6da6468a61eac5b6.jpg")
+routertable = File.open("public/assets/router2-0a295afbc21223617ad777f750162555.jpg")
+jumper = File.open("public/assets/jumper-81ea66dd8fb51c55e49b20a628f5c7f7.jpg")
+snowblower = File.open("public/assets/snowblower-97fb553dc1d771e8a261b483791af089.jpg")
+
+
+
+
+
+
+
+
+
+
+
 resources = [
        
           {
             name: "Tile Cutter",
             description: "Old but tough. Can cut any size tiles. New blades",
             user_id: 2,
-            picture: snowy2
+            picture: tilecutter
+          },
+           {
+            name: "Ladder",
+            description: "20-foot ladder.  Bit dirty, but it does the job!",
+            user_id: 3,
+            picture: ladder
           },
           {
             name: "Router",
             description: "Milwaukee 3-1/2 Max HP Fixed-Base Production ",
             user_id: 2,
-            picture: snowy2
+            picture: router
           },
           {
             name: "Table Saw",
             description: "DEWALT 10 in. Jobsite Table Saw with Rolling Stand",
             user_id: 3,
-            picture: snowy2
+            picture: tablesaw
           },
           {
             name: "Router Table",
             description: "Kreg Precision Router Table System",
             user_id: 4,
-            picture: snowy2
+            picture: routertable
           },
           {
             name: "Jumper",
             description: "Tasco Pro 2200-Amp Jump Start Power Pack and Air Compressor",
             user_id: 1,
-            picture: snowy2
+            picture: jumper
           },
           {
             name: "Snow Blower",
             description: "Sno-Tek 24 in. Two-Stage Electric Start Gas Snow Blower",
             user_id: 2,
-            picture: snowy2
+            picture: snowblower
           }
         
 ]
