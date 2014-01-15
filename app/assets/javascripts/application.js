830368b35f03f99cb0e0b5ba7e793871df6c2126
Elaine Looney <imalooney@gmail.com>
2014-01-15 16:29:20 -0600

    add bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require underscore
 //= require backbone
+//= require bootstrap
 //= require app/util
 //= require app/models/resource
 //= require app/views/resource-view
