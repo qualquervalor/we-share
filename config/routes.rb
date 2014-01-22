058098ff57d6d2535e667625fdc4e67ec1680dbc
Elaine Looney <imalooney@gmail.com>
2014-01-22 15:51:47 -0600

    include new sessions controller in devise paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 WeShare::Application.routes.draw do
-  devise_for :users, :controllers => { registrations: :registrations }
+  devise_for :users, :controllers => { registrations: :registrations, sessions: :sessions }
 
   get 'resources' => 'resources#index'
   get 'resources/:id' => 'resources#show', as: :resources_show
