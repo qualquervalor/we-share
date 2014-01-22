8284da82486a76aaa0d12c53ac8cb0658aa0426c
Elaine Looney <imalooney@gmail.com>
2014-01-22 15:44:15 -0600

    reroute new layout to landing_page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def new
+      
+      build_resource({})
+      render layout: "landing_page"
+      # respond_with self.resource
+
+  end
+
   protected
 
     def after_update_path_for(user)
