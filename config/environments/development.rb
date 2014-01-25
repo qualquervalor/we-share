172e677cd38de248f8d946c0d5fbe9b792cabd78
Qualquer Valor <qualquervalor@gmail.com>
2014-01-24 21:32:08 -0600

    switch smtp server for mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,13 +29,17 @@ WeShare::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :user_name            => ENV["EMAIL_ACCOUNT"],
-  :password             => ENV["EMAIL_PASSWORD"],
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+  config.action_mailer.smtp_settings = { 
+    :address => "localhost", 
+    :port => 1025 
+  }
+  # config.action_mailer.smtp_settings = {
+  # :address              => "smtp.gmail.com",
+  # :port                 => 587,
+  # :user_name            => ENV["EMAIL_ACCOUNT"],
+  # :password             => ENV["EMAIL_PASSWORD"],
+  # :authentication       => "plain",
+  # :enable_starttls_auto => true
+  # }
 
 end
